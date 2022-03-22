c3239af04e200bb17acd446d93150ab3164ffb8d
KenueY <kenueyy@yandex.ru>
2022-03-22 16:40:25 +0700

    Initial

---
+++ ProjectManager/Model/ProjectInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManager.Model
+{
+    class ProjectInfo
+    {
+        string name { get; set; }
+        string customerName { get; set; }
+        string contracterName { get; set; }
+        List<string> workers = new List<string>();
+
+    }
+}
---
+++ ProjectManager/Model/Worker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManager.Model
+{
+    class Worker
+    {
+        string name { get; set; }
+        string surname { get; set; }
+        string patronymic { get; set; }
+        
+    }
+}
